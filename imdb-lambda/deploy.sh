5720a633cdc7c23c0453f908e5a2ed489473a942
Strickland <adstrick@a45e60c5aa6b.ant.amazon.com>
2018-05-08 07:54:55 +1200

    Add region to command line

--- imdb-lambda/deploy.sh
+++ imdb-lambda/deploy.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-S3_Bucket=johns-aws
+S3_Bucket=johns-s3
 
-sam package --template-file template.yaml --s3-bucket $S3_Bucket --output-template-file packaged-template.yaml
-sam deploy --template-file packaged-template.yaml --stack-name imdb-search --capabilities CAPABILITY_IAM
+sam package --template-file template.yaml --s3-bucket $S3_Bucket --output-template-file packaged-template.yaml --region ap-southeast-2
+sam deploy --template-file packaged-template.yaml --stack-name imdb-search --capabilities CAPABILITY_IAM --region ap-southeast-2
